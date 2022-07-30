0b8e84bee604358fd8720adb9e6178c3f199dc65
Zach Lefler <leaving.valhalla@gmail.com>
2022-07-30 12:29:32 -0700

    remove ui packages from json

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/cr/', to: 'creatures#cr'
   get '/environments', to: 'creatures#environments'
   get 'search/:cr/', to: 'creatures#search'
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
 
