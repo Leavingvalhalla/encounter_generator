feafc86b2ad81129191cc270e4d494101c63cd26
Zach Lefler <leaving.valhalla@gmail.com>
2022-07-30 13:33:37 -0700

    refactor autocomplete options

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -1,22 +1,24 @@
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+
 function Type({ onTypeChange }) {
-  let typeOptions = {
-    0: 'monstrous humanoid',
-    1: 'animal',
-    2: 'humanoid',
-    3: 'dragon',
-    4: 'magical beast',
-    5: 'Dragon',
-    6: 'aberration',
-    7: 'plant',
-    8: 'undead',
-    9: 'Ooze',
-    10: 'fey',
-    11: 'outsider',
-    12: 'Outsider',
-    13: 'ooze',
-    14: 'construct',
-    15: 'vermin',
-  };
+  let typeOptions = [
+    { id: 0, name: 'monstrous humanoid' },
+    { id: 1, name: 'animal' },
+    { id: 2, name: 'humanoid' },
+    { id: 3, name: 'dragon' },
+    { id: 4, name: 'magical beast' },
+    { id: 5, name: 'Dragon' },
+    { id: 6, name: 'aberration' },
+    { id: 7, name: 'plant' },
+    { id: 8, name: 'undead' },
+    { id: 9, name: 'Ooze' },
+    { id: 10, name: 'fey' },
+    { id: 11, name: 'outsider' },
+    { id: 12, name: 'Outsider' },
+    { id: 13, name: 'ooze' },
+    { id: 14, name: 'construct' },
+    { id: 15, name: 'vermin' },
+  ];
 
   return (
     <div>
