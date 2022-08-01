0c770b809ddfe09d0bb630f71ff2306fc572b72a
Zach Lefler <leaving.valhalla@gmail.com>
2022-08-01 12:15:59 -0700

    proxy changes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,22 +4,4 @@ class CreaturesController < ApplicationController
         render json: Creature.all
     end
 
-    def show
-        render json: Creature.find(params[:id])
-    end
-
-    def cr
-        render json: Creature.select('distinct(subtype1)')
-    end
-
-    def environments
-        render json: Creature.select('distinct(environment)')
-    end
-
-    def search
-        render json: Creature.where('Cr = ? AND environment = ? AND maintype = ? AND subtype1 = ?', 
-            params[:cr])
-            # , params[:environment], params[:maintype], params[:subtype1])
-    end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-  resources :creatures, only:[:index, :show]
-  get '/cr/', to: 'creatures#cr'
-  get '/environments', to: 'creatures#environments'
-  get 'search/:cr/', to: 'creatures#search'
-  end
+  
+  resources :creatures, only:[:index]
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
