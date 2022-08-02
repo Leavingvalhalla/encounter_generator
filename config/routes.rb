e2d042e6db39b33bb29e66301eda8bb38cf8c09d
Zach Lefler <leaving.valhalla@gmail.com>
2022-08-02 15:20:21 -0700

    remove namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
     resources :creatures, only:[:index]
-  end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
