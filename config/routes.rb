6da88bc54996cf23a8b45314846015e9cca0b02b
Zach Lefler <leaving.valhalla@gmail.com>
2022-08-02 15:13:00 -0700

    add api namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
   resources :creatures, only:[:index, :show]
   get '/cr/', to: 'creatures#cr'
   get '/environments', to: 'creatures#environments'
   get 'search/:cr/', to: 'creatures#search'
+  end
+  
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
